74d54ef6350653f14e9fd8fc48044b96df4e4e5f
Aiden Fisher <fisher.aiden.b@gmail.com>
2026-09-17 15:30:21 -0500

    catalog page structuring

--- Navigation/course_catalog.py
+++ Navigation/course_catalog.py
@@ -6,6 +6,19 @@ from Navigation.navbar import navbar
 @ui.page("/courses")
 def course_catalog():
     navbar()
-    with ui.column().classes("items-center gap-4 w-full mt-24"):
-        ui.label("This will have some courses info").classes("text-3xl font-semibold")
+
+    with ui.row().classes("w-full flex-wrap justify-center items-stretch gap-6"):
+        with ui.card().classes("w-full md:flex-1 md:max-w-md"):
+            ui.image('https://picsum.photos/id/684/640/360')
+            with ui.card_section().classes("flex flex-col"):
+                ui.label('Course Title').classes('text-lg text-slate-700')
+                ui.label('Course Location').classes('text-xs text-slate-500')
+                ui.label('Course Difficulty').classes('text-xs text-slate-500')
+
+        with ui.card().classes("w-full md:flex-1 md:max-w-md"):
+            ui.image('https://picsum.photos/id/684/640/360')
+            with ui.card_section():
+                  ui.label('Course Title').classes('text-lg text-slate-700')
+                  ui.label('Course Location').classes('text-xs text-slate-500')
+                  ui.label('Course Difficulty').classes('text-xs text-slate-500')
 
